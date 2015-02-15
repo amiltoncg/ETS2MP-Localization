0ab90c42f353c625e2daa41ff851035c3dfcc566
amiltoncg <amiltonkampos@hotmail.com>
2015-02-15 16:18:25 -0200

    Create portuguese.php

---
+++ portuguese.php
@@ -0,0 +1,110 @@
+<?php
+//
+// ETS2MP Homepage language file.
+// File: portuguese.php
+// Language name: Portuguese
+// Translated name: Português
+//
+
+$_lang["download"] = "Baixar";
+$_lang["home"] = "Home";
+$_lang["status"] = "Status";
+$_lang["username"] = "Usuário";
+$_lang["password"] = "Senha";
+$_lang["login"] = "Login";
+$_lang["register"] = "Registrar";
+$_lang["download.latest.version"] = "Baixar versão mais recente";
+$_lang["current.release"] = "Versão atual:";
+$_lang["language"] = "Idioma";
+$_lang["join"] = "JUNTAR-SE";
+$_lang["mainpage.join"] = "Junte-se a milhares de entuasistas de caminhão online!"; //to review
+$_lang["ts3.advertisement"] = "Interação de voz com outros motoristas - clique para participar do nosso servidor TS3";
+$_lang["facebook.advertisement"] = "Últimas notícias em nosso facebook.";
+$_lang["registration.blocked"] = "Cadastramento está bloqueado - nós ainda estamos trabalhando em algumas funções que nos forçaram a bloquear o cadastramento. Vamos desbloquear quando tudo estiver pronto e estaremos aptos para publicar uma nova versão."; //to review
+$_lang["invalid.username.password"] = "E-mail ou senha inválidos.";
+$_lang["already.logged"] = "Você já está logado.";
+$_lang["welcome"] = "Bem-vindo";
+$_lang["logout"] = "Sair";
+$_lang["bans"] = "Bans";
+$_lang["version"] = "Versão";
+$_lang["update.time"] = "Data da atualização";
+$_lang["req.game.version"] = "Versão de jogo requerida";
+$_lang["requires"] = "Requer";
+$_lang["or.newer"] = "ou mais recente";
+$_lang["game.client"] = "Game Cliente";
+$_lang["download.no.permissions"] = "Para baixar o mod você precisa estar logado.";
+$_lang["why.create.account"] = "Por que criar uma conta?<br />Uma conta será usada para conectar-se a todos os serviços (fórum, jogo, economia e outros). Pretendemos usá-los para introduzir estatísticas econômicas e pessoais dos motoristas no futuro. Você será capaz de modificar o seu próprio perfil."; //to review
+$_lang["website.title"] = "ETS2MP - Virtual trucking world - multiplayer mod for Euro Truck Simulator 2 game by SCS Software"; //to review
+$_lang["cookies.warning"] = "Este site usa <a href=\"http://en.wikipedia.org/wiki/HTTP_cookie\">cookies</a>. Ao continuar a navegar no site, você está concordando com a nossa utilização de cookies.";
+$_lang["forum"] = "Fórum";
+$_lang["cannot.create.account.when.logged"] = "Você não pode criar uma nova conta enquanto estiver logado.";
+$_lang["successfully.registered"] = "Você foi registrado com sucesso. Por favor verifique seu e-mail para ativar sua conta. Email: <b>{0}</b>";
+$_lang["error.unknown"] = "Erro desconhecido";
+$_lang["credentials.taken"] = "Usuário, email ou steamid já cadastrado.";
+$_lang["error.no.steam.data"] = "Para criar uma conta ETS2MP você deve ter o ETS2 atribuído à sua conta Steam. Se você o tem em sua biblioteca Steam tente verificar se o seu perfil é público. Se ele está privado torne-o público e pressione"; //to review
+$_lang["register.steam.info"] = "Para registrar você tem que conectar à sua conta Steam. Clique no botão abaixo para redirecionar para o site de autenticação Steam.";
+$_lang["rules.info"] = "Quando você pressiona o botão de registro você automaticamente aceita nossas regras da comunidade. Para verificar elas clique aqui.";
+$_lang["mail"] = "E-Mail";
+$_lang["repeat.mail"] = "Repita o email";
+$_lang["new.password"] = "Nova senha";
+$_lang["repeat.new.password"] = "Repita a nova senha";
+$_lang["new.password.summary"] = "Se você deixar esses campos em branco a senha não mudará.";
+$_lang["old.password"] = "Senha antiga";
+$_lang["repeat.old.password"] = "Repita a senha antiga";
+$_lang["save.account.data"] = "Salvar informações de conta";
+$_lang["edit.account"] = "Editar conta";
+$_lang["forgot.password"] = "Esqueceu a senha? Clique <a href=\"index.php?page=resetPassword\">aqui</a>.";
+$_lang["forgot.password.summary"] = "Você esqueceu a senha? Digite seu email abaixo e pressione o botão \"Lembrar senha\".";
+$_lang["reminder.button"] = "Lembrar senha";
+$_lang["reset.password"] = "Resetar senha";
+$_lang["password.reminder.mail.title"] = "Pedido de lembrete de senha.";
+$_lang["password.reminder.content"] = "Olá.\n\nAlguém de ip {0} solicitou o resetamento de senha para a sua conta ETS2MP.\n\nPara resetar sua senha clique nesse link: {1}\nSe não foi você ignore esse email.\n\nSaudações\nETS2MP Team";
+$_lang["new.password.mail.title"] = "Sua nova senha.";
+$_lang["new.password.mail.content"] = "Olá.\n\nEssa é sua nova senha: {0}\n\nLembre-se de mudá-la imediatamente após conectar-se.\n\nSaudações\nETS2MP Team";
+$_lang["account.activated"] = "Sua conta foi ativada com sucesso.";
+$_lang["unable.to.activate"] = "Impossível ativar a conta, chave inválida ou a conta já está ativada.";
+$_lang["not.activated"] = "Sua conta ainda não está ativada. Se você não tem a mensagem de ativação clique <a href=\"index.php?action=resendActivation\">aqui</a> para reenviar.";
+$_lang["force.password.change"] = "Por favor mude sua senha imediatamente. Clique <a href=\"index.php?page=editAccount\">aqui</a> para mudar.";
+$_lang["forgot.password.link.sent"] = "Verifique seu e-mail. Nós enviamos a você um link para resetar a senha.";
+$_lang["forgot.password.sent"] = "Verifique seu e-mail. Nós enviamos a você uma nova senha.";
+$_lang["forgot.password.invalid.key"] = "Chave de resetamento de senha inválida.";
+$_lang["activation.key.resent"] = "Uma nova chave de ativação foi enviada para o seu e-mail.";
+$_lang["activation.mail.title"] = "Ativação de conta ETS2MP.";
+$_lang["activation.mail.content"] = "Olá.\n\nEssa é sua chave de ativação: {0}\n\nSaudações\nETS2MP Team";
+$_lang["no.login"] = "Você precisa estar logado para acessar esse website.";
+$_lang["settings.saved.with.mail"] = "A conta foi salva. Verifique seu novo e-mail e ative a conta novamente.";
+$_lang["new.email.in.use"] = "Este email já está em uso.";
+$_lang["email.does.not.match"] = "Email fornecido não corresponde."; //to review
+$_lang["invalid.email.format"] = "O e-mail que você informou é inválido.";
+$_lang["new.password.does.not.match"] = "A nova senha que você colocou não corresponde.";
+$_lang["too.short.password"] = "A nova senha é muito curta. Mínimo: 5 caracteres";
+$_lang["old.password.does.not.match"] = "Senha antiga não corresponde.";
+$_lang["settings.saved"] = "A conta foi salva.";
+$_lang["invalid.old.password"] = "A senha antiga é inválida, impossível salvar a conta.";
+$_lang["no.password.change.forced"] = "Para resetar a senha você deve estar desconectado.";
+$_lang["password.reminder.edit.account"] = "Não lembra a senha? Clique <a href=\"index.php?page=resetPassword\">aqui</a>.";
+$_lang["contact"] = "Contate";
+$_lang["your.mail"] = "Seu e-mail";
+$_lang["content"] = "Conteúdo";
+$_lang["send"] = "Enviar";
+$_lang["contact.message.sent"] = "Obrigado por entrar em contato. Por favor, aguarde pela resposta.";
+$_lang["topic"] = "Tópico";
+$_lang["contact.message.too.short"] = "Mensagem é muito curta. Mínimo 10 caracteres.";
+$_lang["contact.topic.too.short"] = "Tópico é muito curto. Mínimo 10 caracteres.";
+$_lang["invalid.mail.forgot.password"] = "Nós somos incapazes de encontrar qualquer conta com este email.";
+$_lang["steamid.in.use"] = "Nós encontramos uma conta ETS2MP já conectada à essa conta Steam. Se você tiver qualquer problema com o registro, por favor <a href=\"http://ets2mp.com/support/\">crie um ticket de suporte</a>.";
+$_lang["mail.does.not.match"] = "Endereço de e-mail informado não corresponde.";
+$_lang["support"] = "Suporte";
+$_lang["profile"] = "Perfil";
+$_lang["group"] = "Grupo";
+$_lang["banned"] = "Banido";
+$_lang["state"] = "Estado";
+$_lang["account.is.activated"] = "Conta está ativada";
+$_lang["account.isnt.activated"] = "Conta não está ativada";
+$_lang["penalty.history"] = "Histórico de penalidades";
+$_lang["no.punishments"] = "Nenhuma punição para mostrar.";
+$_lang["date"] = "Data";
+$_lang["reason"] = "Razão";
+$_lang["type"] = "Tipo";
+$_lang["penalty.limit"] = "Somente o proprietário deste perfil pode ver mais do histórico de penalidades.";
+$_lang["error.cannot.fetch.steam.api"] = "Impossível conectar a steam api. Tente novamente depois.";
